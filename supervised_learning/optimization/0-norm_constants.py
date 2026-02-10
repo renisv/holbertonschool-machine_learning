bb69a32be7c2bb2fe4da53b4d106f9c55ad99fab
abfabs <albaeftimi@gmail.com>
2026-02-10 12:30:13 +0100

    pycode

--- supervised_learning/optimization/0-norm_constants.py
+++ supervised_learning/optimization/0-norm_constants.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 import numpy as np
 
+
 def normalization_constants(X):
     """
     Calculates the mean and standard deviation of each feature in X
 
     X: numpy.ndarray of shape (m, nx)
-    Returns: mean, std (both numpy.ndarray of shape (nx,))
+    Returns: mean and standard deviation of each feature
     """
     m = np.mean(X, axis=0)
     s = np.std(X, axis=0)
