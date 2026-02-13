1521b69d89cdb6ffd0fe1a78eec7b7e7813d00b3
abfabs <albaeftimi@gmail.com>
2026-02-13 23:38:21 +0100

    output

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -18,9 +18,15 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
     Returns:
         Output of the new layer
     """
+    initializer = tf.keras.initializers.VarianceScaling(
+        scale=2.0, mode="fan_avg"
+    )
+
     layer = tf.keras.layers.Dense(
         units=n,
         activation=activation,
+        kernel_initializer=initializer,
         kernel_regularizer=tf.keras.regularizers.l2(lambtha)
     )
+
     return layer(prev)
