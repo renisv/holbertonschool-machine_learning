6aa136992b30c8d098df43700f49f1018ea7e30f
abfabs <albaeftimi@gmail.com>
2026-02-14 00:11:51 +0100

    hmmm

--- supervised_learning/regularization/5-dropout_gradient_descent.py
+++ supervised_learning/regularization/5-dropout_gradient_descent.py
@@ -1,43 +1,55 @@
 #!/usr/bin/env python3
-
+'''
+Modulus that  updates the weights of a neural network with
+Dropout regularization using gradient descent
+'''
 import numpy as np
 
+
 def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
-    """
-    Performs one pass of gradient descent on the weights of a deep neural network
-    with dropout and L2 regularization.
-    
-    Arguments:
-    Y -- one-hot labels, shape (classes, m)
-    weights -- dictionary of weights and biases
-    cache -- dictionary of activations and dropout masks
-    alpha -- learning rate
-    keep_prob -- probability of keeping a neuron active
-    L -- number of layers
+    '''
+    Function  that updates the weights of a neural network with
+    Dropout regularization using gradient descent
 
-    Returns:
-    weights -- updated weights dictionary
-    """
+    Parameters
+    ----------
+    Y : TYPE numpy.ndarray
+        DESCRIPTION. Y is a one-hot numpy.ndarray of shape (classes, m)
+        that contains the correct labels for the data
+    weights : TYPE dictionary
+        DESCRIPTION. Dictionary of the weights and biases of the neural network
+    cache : TYPE dictionary
+        DESCRIPTION. Dictionary of the outputs and dropout masks of
+        each layer of the neural network
+    alpha : TYPE float
+        DESCRIPTION. Learning rate
+    keep_prob : TYPE float
+        DESCRIPTION. Probabily that a node will be kept
+    L : TYPE int
+        DESCRIPTION. Number of layers of DNN
+
+    Returns
+    -------
+    None.
+
+    '''
     m = Y.shape[1]
-    lambtha = 0.1  # default L2 regularization parameter for autograder
-
-    for l in reversed(range(1, L + 1)):
-        A_prev = cache['A' + str(l - 1)]
-        A = cache['A' + str(l)]
-        W = weights['W' + str(l)]
-        b = weights['b' + str(l)]
-
-        if l == L:
-            dZ = A - Y
+    for i in reversed(range(1, L + 1)):
+        w = weights['W' + str(i)]
+        b = weights['b' + str(i)]
+        a0 = cache['A' + str(i - 1)]
+        a = cache['A' + str(i)]
+        if i == L:
+            dz = a - Y
+            W = w
         else:
-            dA = np.dot(weights['W' + str(l + 1)].T, dZ) * cache['D' + str(l)]
-            dA /= keep_prob
-            dZ = dA * (A * (1 - A))
-
-        dW = np.dot(dZ, A_prev.T)/m + (lambtha/m)*W
-        db = np.sum(dZ, axis=1, keepdims=True)/m
-
-        weights['W' + str(l)] = W - alpha * dW
-        weights['b' + str(l)] = b - alpha * db
-
-    return weights
+            d = cache['D' + str(i)]
+            da = 1 - (a * a)
+            dz = np.matmul(W.T, dz)
+            dz = dz * da * d
+            dz = dz / keep_prob
+            W = w
+        dw = np.matmul(a0, dz.T) / m
+        db = np.sum(dz, axis=1, keepdims=True) / m
+        weights['W' + str(i)] = w - alpha * dw.T
+        weights['b' + str(i)] = b - alpha * db
